ad991daef156f0a674f7dd23966642a750863a0a
D1van007 <d1van28.01.1987@gmail.com>
2022-11-07 20:30:41 +0300

    feat: finish task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,8 @@
-const { readdir, stat } = require('fs/promises');
+const { readdir} = require('fs/promises');
+const { createReadStream, createWriteStream} = require('fs');
 const path = require('path');
 const pathStylesFolder = path.join(__dirname, 'styles');
+const pathBundleFile = path.join(__dirname, 'project-dist/bundle.css');
 
 const bundleStyles = async () => {
     try {
@@ -8,13 +10,14 @@ const bundleStyles = async () => {
 
         for ( let i = 0; i < styleFiles.length; i += 1) {
         const pathStylesFile = path.join(pathStylesFolder, styleFiles[i]);
-        const statStylesFile = await stat(pathStylesFile);
         const StylesFileExt = path.extname(pathStylesFile);
-        const StylesFileName = path.basename(pathStylesFile, StylesFileExt);
 
-            if (StylesFileExt === '.css'){
-    console.log(StylesFileName)
-            }
+        const inStyle = createReadStream(pathStylesFile, 'utf-8');
+        const writeInBundle = createWriteStream(pathBundleFile, {flags: 'a+'});
+ 
+                if (StylesFileExt === '.css')  {
+                    inStyle.pipe(writeInBundle)
+                }
         };
       } catch (err) {
         console.error(err);
