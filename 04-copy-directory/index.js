f0cb3af02f9cd53138c183a0a7db691765ff8273
D1van007 <d1van28.01.1987@gmail.com>
2022-11-07 16:26:00 +0300

    feat: finish task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const { mkdir,  readdir, unlink} = require('fs/promises');
+const {createReadStream, createWriteStream} = require('fs');
+const path = require('path');
+const pathOriginFolder = path.join(__dirname, 'files')
+const pathCloneFolder = path.join(__dirname, 'files-copy')
 
+
+const copyFolders = async () => {
+    try {    
+        const cloneFolder = await mkdir(pathCloneFolder, { recursive: true });
+        const originFiles = await readdir(pathOriginFolder);
+        const cloneFiles = await readdir(pathCloneFolder);
+
+            for ( let i = 0; i < cloneFiles.length; i += 1) {
+                const pathCloneFileForDel = path.join(pathCloneFolder, cloneFiles[i]);
+                unlink(pathCloneFileForDel);
+            };
+
+            for ( let i = 0; i < originFiles.length; i += 1) {
+                    const pathOriginFile = path.join(pathOriginFolder, originFiles[i]);
+                    const pathCloneFile = path.join(pathCloneFolder, originFiles[i]);
+           createReadStream(pathOriginFile).pipe(createWriteStream(pathCloneFile));
+        };
+            
+      } catch (err) {
+        console.error(err);
+      };
+};
+copyFolders();
